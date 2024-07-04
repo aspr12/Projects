2eb499d7761c04e0fb320dfb0766270111a484cc
Arthur <154482103+aspr12@users.noreply.github.com>
2024-07-04 16:53:46 -0300

    Add files via upload

---
+++ Text Note.py
@@ -0,0 +1,132 @@
+import os
+from tkinter import *
+from tkinter import filedialog, colorchooser, font
+from tkinter.messagebox import showinfo
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+def change_color():
+    color = colorchooser.askcolor(title="pick a color... or else")
+    text_area.config(fg=color[1])
+
+def change_font(*args):
+    text_area.config(font=(font_name.get(), font_size.get()))
+
+def new_file():
+    global file
+    window.title("Untitled - Text Editor")
+    file = None
+    text_area.delete(1.0, END)
+
+def open_file():
+    global file
+    file = askopenfilename(defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                      ("Text Documents", "*.txt")])
+    if file == "":
+        file = None
+    else:
+        window.title(os.path.basename(file) + " - Text Editor")
+        text_area.delete(1.0, END)
+        with open(file, "r") as f:
+            text_area.insert(1.0, f.read())
+
+def save_file():
+    global file
+    if file is None:
+        file = asksaveasfilename(initialfile='Untitled.txt',
+                                 defaultextension=".txt",
+                                 filetypes=[("All Files", "*.*"),
+                                            ("Text Documents", "*.txt")])
+        if file == "":
+            file = None
+        else:
+            with open(file, "w") as f:
+                f.write(text_area.get(1.0, END))
+            window.title(os.path.basename(file) + " - Text Editor")
+    else:
+        with open(file, "w") as f:
+            f.write(text_area.get(1.0, END))
+
+def cut():
+    text_area.event_generate("<<Cut>>")
+
+def copy():
+    text_area.event_generate("<<Copy>>")
+
+def paste():
+    text_area.event_generate("<<Paste>>")
+
+def about():
+    showinfo("About", "This is a simple text editor built with Tkinter.")
+
+def quit():
+    window.destroy()
+
+window = Tk()
+window.title("Text editor program")
+file = None
+
+window_width = 650
+window_height = 650
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
+
+window.geometry("{}x{}+{}+{}".format(window_width, window_height, x, y))
+
+font_name = StringVar(window)
+font_name.set("Arial")
+
+font_size = StringVar(window)
+font_size.set("25")
+
+text_area = Text(window, font=(font_name.get(), font_size.get()))
+
+scroll_bar = Scrollbar(text_area)
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(0, weight=1)
+text_area.grid(sticky=N + E + S + W)
+
+frame = Frame(window)
+frame.grid()
+
+color_button = Button(frame, text="color", command=change_color)
+color_button.grid(row=0, column=0)
+
+font_box = OptionMenu(frame, font_name, *font.families(), command=change_font)
+font_box.grid(row=0, column=1)
+
+size_box = Spinbox(frame, from_=1, to=100, textvariable=font_size, command=change_font)
+size_box.grid(row=0, column=2)
+
+scroll_bar.pack(side=RIGHT, fill=Y)
+text_area.config(yscrollcommand=scroll_bar.set)
+
+# Menu bar
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+# File menu
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=quit)
+
+# Edit menu
+edit_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut", command=cut)
+edit_menu.add_command(label="Copy", command=copy)
+edit_menu.add_command(label="Paste", command=paste)
+
+# Help menu
+help_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="About", command=about)
+
+window.mainloop()
